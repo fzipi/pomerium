fecad04e09314a3787ede4380c0ceb9b7be97d92
Travis Groth <travisgroth@users.noreply.github.com>
2019-08-28 14:14:51 -0400

    i hate you, dockerhub

---
+++ .travis/docker_build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+IMAGE_NAME=${TRAVIS_REPO_SLUG}
+
+for ARCH in amd64 arm64v8 arm32v7 arm64v6; do
+    case $ARCH in
+    amd64)
+        DOCKERFILE='Dockerfile'
+        ;;
+    *)
+        DOCKERFILE="Dockerile.$ARCH"
+        ;;
+    esac
+
+    FULL_NAME_NAME=${IMAGE_NAME}:${ARCH}-${TRAVIS_BRANCH}
+    docker build -t "${FULL_NAME_NAME}" -f ${DOCKERFILE} .
+    echo FULL_NAME_NAME >>"/tmp/${TRAVIS_COMMIT}/images"
+
+    ## DEBUG
+    docker version
+    docker inspect "${FULL_NAME_NAME}"
+    cat "/tmp/${TRAVIS_COMMIT}/images"
+done
